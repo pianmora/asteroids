17e3e5712a8d32775200b8f22d51ef3332bd008e
pianmora <martin.eichner@gmail.com>
2024-10-29 23:10:58 +0100

    Fixed main game loop to correctly draw and cycle

--- main.py
+++ main.py
@@ -11,15 +11,29 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    # Set up the main game screen
+    game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Set up the game clock
+    game_clock = pygame.time.Clock()
+
+    dt = 0
+
     while True:
+        # Allow user to close game on close button on title bar
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        screen.fill(color=pygame.Color(0,0,0))
+
+        # redraw screen    
+        game_screen.fill((80, 80, 80))
+
+        # Flip the display to render the updated screen
         pygame.display.flip()
+
+        # Delta adjust to 60fps
+        dt = game_clock.tick(60) / 1000
+        print(dt)
 
 if __name__ == "__main__":
     main()
