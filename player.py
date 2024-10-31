9ca539667cf7e3e72fdb7bb8a9d018e726cb9488
pianmora <martin.eichner@gmail.com>
2024-10-31 21:05:55 +0100

    forwards/backwards movement added and tested

--- player.py
+++ player.py
@@ -18,15 +18,29 @@ class Player(CircleShape):
     def draw(self, screen):
         pygame.draw.polygon(screen, (230,230,230), self.triangle(), 2)
 
-    def rotate(self, dt, direction):
-        self.rotation += (PLAYER_TURN_SPEED * dt) * direction
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
         self.rotation %= 360
-        
+    
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
-            self.rotate(dt, -1)
+            # left
+            self.rotate(-dt)
 
         if keys[pygame.K_d]:
-            self.rotate(dt, 1)
+            # right
+            self.rotate(dt)
+        
+        if keys[pygame.K_w]:
+            # forward
+            self.move(dt)
+        
+        if keys[pygame.K_s]:
+            # backward
+            self.move(-dt)
