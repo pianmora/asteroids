eed4926a8760901cbed546a5cd2ce83ea96e0153
pianmora <martin.eichner@gmail.com>
2024-11-02 09:20:46 +0100

    fixed warnings in code due to pygame libraries

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-import pygame
+import pygame #type:ignore
 from constants import *
 from player import Player
 from asteroid import Asteroid
--- shot.py
+++ shot.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame #type:ignore
 from constants import *
 from circleshape import CircleShape
 
